07fedcbe75f48db78639bc171041875e741bdc3a
Maciej <maciejwsrh97@wp.pl>
2020-04-11 01:52:09 -0700

    Updates

---
+++ precache-manifest.2afcc732474d3cad244a68a5fcf7441c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9f857f44194a8b6a70c02e655d4da1",
+    "url": "/AdidasStore/index.html"
+  },
+  {
+    "revision": "0cc5f137918fc660d33a",
+    "url": "/AdidasStore/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2d6f46db85fb71214a0e",
+    "url": "/AdidasStore/static/css/main.f9a81e14.chunk.css"
+  },
+  {
+    "revision": "0cc5f137918fc660d33a",
+    "url": "/AdidasStore/static/js/2.c294e901.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AdidasStore/static/js/2.c294e901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d6f46db85fb71214a0e",
+    "url": "/AdidasStore/static/js/main.77346ba5.chunk.js"
+  },
+  {
+    "revision": "379d3afead4abb3f7578",
+    "url": "/AdidasStore/static/js/runtime-main.c48372d2.js"
+  },
+  {
+    "revision": "aa80cfa2c50c1d15c100fca8b83e4a8c",
+    "url": "/AdidasStore/static/media/Paul.aa80cfa2.ttf"
+  },
+  {
+    "revision": "aa9925ad75ebd634a562304eac33712d",
+    "url": "/AdidasStore/static/media/cart.aa9925ad.png"
+  },
+  {
+    "revision": "ce0d19379ec5c9cefdff4af106ca98d1",
+    "url": "/AdidasStore/static/media/favourite.ce0d1937.png"
+  },
+  {
+    "revision": "0de565b10153351a11b682587df4fbc3",
+    "url": "/AdidasStore/static/media/shoes.0de565b1.jpg"
+  },
+  {
+    "revision": "c05b1119a56c2a0e4c0856d9e09c84d3",
+    "url": "/AdidasStore/static/media/shoes2.c05b1119.jpg"
+  }
+]);
